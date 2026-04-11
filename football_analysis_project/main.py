3f6ef195a6fe35373ec1934f0179994a161e912d
SOHAN-SAHA-2005 <sohansaha5r55@gmail.com>
2026-04-11 17:10:03 +0530

    changes

--- football_analysis_project/main.py
+++ football_analysis_project/main.py
@@ -1,12 +1,26 @@
 from utils import read_video, save_video
+from trackers import Tracker
+
 
 
 def main():
-    # read video
+     # read video
     video_frames = read_video("input_videos/08fd33_4.mp4")
 
+    #initialize tracker
+    tracker = Tracker('models/best1.pt')
+
+    tracks = tracker.get_object_tracks(video_frames,
+                                       read_from_stub=False,
+                                       stub_path="stubs/tracks_stub.pkl")
+    
+    #draw object tracks
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+
+
     #save video
-    save_video(video_frames, "output_videos/output_videos.avi")
+    save_video(output_video_frames, "output_videos/output_video.avi")
+
 
 if __name__ == "__main__":
     main()
